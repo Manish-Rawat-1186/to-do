4f66205db7554d6b7337bc010cc7643dc5aee3a0
Manish-Rawat-1186 <manishrawat1186@gmail.com>
2022-06-14 00:03:31 +0530

    frontend-complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import './App.scss'
 
 const App = () => {
+
+  const [task, setTask] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
-    <section class="container">
-    <div class="heading">
-      <h1 class="heading__title">To-Do List</h1>
+    <section className="container">
+    <div className="heading">
+      <h1 className="heading__title">To-Do List</h1>
     </div>
-    <form class="form">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const trimmedText = task.trim()
+        if (trimmedText.length > 0) {
+        setTodos([...todos, trimmedText])
+        setTask('');
+      }
+      }}
+     className="form">
       <div>
-        <label class="form__label" for="todo">~ Today I need to ~</label>
-        <input class="form__input"
+        <label className="form__label" htmlFor="todo">~ Today I need to ~</label>
+        <input className="form__input"
              type="text" 
+             value={task}
              id="todo" 
              name="to-do"
              size="30"
+             onChange={e => {
+               setTask(e.target.value)
+               }
+               }
              required />
-        <button class="button"><span>Submit</span></button>
+        <button type='submit' className="button">
+        <span>Submit</span>
+        </button>
       </div>
     </form>
     <div>
-      <ul class="toDoList">
+      <ul className="toDoList">
+       {
+         todos.map((todo, index) => (
+           <li 
+           onClick={(e) => {
+              const newTodos = todos.filter((_,todo) => todo !== index)
+
+              setTodos(newTodos);
+           }} key={index}
+           >{todo}  </li>
+         ))
+       }
       </ul>
     </div>
   </section>
